c654d0761358e27997e6d3880cbe08212c39ae83
step21 <step21@devtal.de>
2022-08-15 13:04:00 +0200

    added fake tests - for now there are only doctest tests

--- tests/test_eurlex.py
+++ tests/test_eurlex.py
@@ -1,5 +1,21 @@
 from eurlex import __version__
 
-
+# This test is just a placeholder to make sure the tests are running
 def test_version():
     assert __version__ == "0.1.0"
+
+
+def test_make_query():
+    assert True
+
+
+def test_run_query():
+    assert True
+
+
+def test_curia_cases():
+    assert True
+
+
+def test_curia_scraper():
+    assert True
