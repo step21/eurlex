a01529a7d8965eec3cdf5d4c104851d2ae4f31db
step21 <step21@devtal.de>
2022-08-28 23:33:07 +0200

    bumped version number for tests to 0.1.6

--- tests/test_eurlex.py
+++ tests/test_eurlex.py
@@ -2,7 +2,7 @@ from eurlex import __version__
 
 # This test is just a placeholder to make sure the tests are running
 def test_version():
-    assert __version__ == "0.1.5"
+    assert __version__ == "0.1.6"
 
 
 def test_make_query():
