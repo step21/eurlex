b706b08f65671f4b032e9425ca371b3e3b6b0b92
step21 <step21@devtal.de>
2022-09-07 16:50:40 +0200

    bumped version

--- eurlex/__init__.py
+++ eurlex/__init__.py
@@ -1,4 +1,4 @@
 """
 * Python module to create eurlex cellar queries, query eurlex for metadata of documents with sparql queries, and subsequently download associated documents and notices.
 """
-__version__ = "0.2.2"
+__version__ = "0.2.3"
